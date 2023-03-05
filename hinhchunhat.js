01e8042d0e23a1081aa7965c2ecd73df6779836a
KanJavi <125029223+KanJavi@users.noreply.github.com>
2023-03-05 10:32:50 +0900

    Add files via upload

---
+++ hinhchunhat.js
@@ -0,0 +1,4 @@
+let width =parseInt(prompt('nhập vào chiều rộng hình chữ nhật:'));
+let heigth = parseInt(prompt('nhập vào chiều dài của hình chữ nhật:'));
+let area = width*heigth;
+document.write("diện tích hình chữ nhật là:",+area)
